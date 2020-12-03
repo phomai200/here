8522d1a3717f02a8b15228fd84e92ee811dfd5d7
phomai200 <75442404+phomai200@users.noreply.github.com>
2020-12-03 17:53:08 +0000

    Add files via upload

---
+++ Receiver.py
@@ -0,0 +1,28 @@
+#RECEIVER
+
+from cryptography.fernet import Fernet
+import paho.mqtt.client as mqtt
+import base64, os, time
+
+#key = Fernet.generate_key()
+#print(key)
+# b'eeVnw8VV5EKdDFPsCq7Ubk25z8NOAwcru4P-twyemF0='
+
+key = b'eeVnw8VV5EKdDFPsCq7Ubk25z8NOAwcru4P-twyemF0='
+cipher = Fernet(key)
+
+def alert(client, userdata, message):
+    message = message.payload.decode("utf-8")
+    decrypt = cipher.decrypt(message.encode("utf-8"))
+    print(decrypt.decode("utf-8")+"\n")
+
+serv = "broker.hivemq.com"
+cli = mqtt.Client()
+cli.connect(serv)
+
+while True:
+    cli.loop_start()
+    cli.subscribe("12/30")
+    cli.on_message = alert
+    time.sleep(60)
+    cli.loop_stop()
